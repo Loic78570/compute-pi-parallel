ddadbdce2ba7c511d268caa64ce9b3f336d43718
Loïc Gavens <gavensloic@eisti.fr>
2022-04-30 22:59:04 +0100

    recheck second algo (weird check)

--- src/assignment02/assignment2.c
+++ src/assignment02/assignment2.c
@@ -1,9 +1,13 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdbool.h>
+#include <sys/time.h>
+#include <omp.h>
 
 #define ROWS 3
 #define COLUMNS 3
+
+
 
 //char **genmatrix(){
 //
@@ -19,13 +23,23 @@
 
 int main(){
 
+    omp_set_num_threads(omp_get_num_procs());
 
     int i,j;
-    char a[COLUMNS][ROWS];
+    static char a[COLUMNS][ROWS] = {{'a', 'b', 'c'},
+                                    {'d', 'e', 'f'},
+                                    {'g', 'h', 'a'}};
 
-    for(i = 0;i < ROWS;i++)
-        for(j = 0;j < COLUMNS;j++)
-            a[i][j] = (rand() % 26) + 'A';
+    struct timeval tv1, tv2;
+    struct timezone tz;
+    double elapsed;
+
+    //GENERATION
+//    for(i = 0;i < ROWS;i++)
+//        for(j = 0;j < COLUMNS;j++)
+//            a[i][j] = (rand() % 26) + 'A';
+
+   // PRINTING
 
     for(i = 0;i < ROWS;i++){
         for(j = 0;j < COLUMNS;j++){
@@ -36,11 +50,14 @@ int main(){
         }
     }
 
+    gettimeofday(&tv1, &tz);
+
 // searching for left to right palindromes
 
     int ind_palin = 1;
     int num_palins = 0;
 
+#pragma omp parallel for private(i,j) shared(ind_palin,num_palins)
     for(i = 0;i < ROWS;i++){
         for(j = 0;j < COLUMNS;j++){
 //            printf("%c  ", a[i][j]);
@@ -57,6 +74,7 @@ int main(){
 
     // search for top to bottom palindroms
 
+#pragma omp parallel for private(i,j) shared(ind_palin,num_palins)
     for(i = 0;i < ROWS;i++){
         for(j = 0;j < COLUMNS;j++){
 //            printf("%c  ", a[i][j]);
@@ -73,104 +91,66 @@ int main(){
 
     // search for crossed palindrom (top right to bottom left)
 
-//    i = 0;
-//    j = COLUMNS-1;
-////    for(i = 0;i < ROWS;i++){
-//    while(i<ROWS){
-////        for(j = COLUMNS-1;j >= 0;j--){
-//        while(j>=0){
-//            if((i!=0) && (j!=0)){
-//                int ibis = i;
-//                int jbis = j;
-//                while((ibis!=0) && (jbis!=0)){
-////                    printf("ibis : %d, jbis: %d\n ", ibis, jbis);
-//
-//                    if(ibis<ROWS){
-//                        printf("comparing : %c, %c\n", a[ibis][jbis], a[ibis-1][jbis-1]);
-//                        if(a[ibis][jbis]==a[ibis-1][jbis-1]){
-//                            ind_palin++;
-//                        }
-//                        else if(ind_palin>1){
-//                            num_palins++;
-//                            ind_palin = 1;
-//                        }
-//                    }
-//
-//                    ibis--;
-//                    jbis--;
-//                }
-//
-//            }
-//
-//            i++;
-//            if(i==COLUMNS+1){
-//                break;
-//            }
-//        }
-//        j--;
-//    }
+    i = 0;
+    j = COLUMNS-1;
+//    for(i = 0;i < ROWS;i++){
+#pragma omp parallel while private(i,j, ibis, jbis) shared(ind_palin,num_palins)
+    while(i<ROWS){
+//        for(j = COLUMNS-1;j >= 0;j--){
+        while(j>=0){
+            if((i!=0) && (j!=0)){
+                int ibis = i;
+                int jbis = j;
+                while((ibis!=0) && (jbis!=0)){
+//                    printf("ibis : %d, jbis: %d\n ", ibis, jbis);
+
+                    if(ibis<ROWS){
+                        printf("comparing : %c, %c\n", a[ibis][jbis], a[ibis-1][jbis-1]);
+                        if(a[ibis][jbis]==a[jbis][ibis] && jbis!=ibis){
+                            ind_palin++;
+                        }
+                        else if(ind_palin>1){
+                            num_palins++;
+                            ind_palin = 1;
+                        }
+                    }
+
+                    ibis--;
+                    jbis--;
+                }
+
+            }
+
+            i++;
+            if(i==COLUMNS+1){
+                break;
+            }
+        }
+        j--;
+    }
 
     printf("XXX\n");
     //search for top left to bottom right
 
-//    i = 1;
-//    j = 0;
-//    int e = 0;
-//    int n = 4;
-//    int cntr = 0;
-//
-//    for(int i = 1; i<=n; i++){
-//        while(j<i){
-//            printf("I,J : %d,%d\n", i, j);
-//            int ibis = i+e;
-//            int jbis = j-e;
-//            while(ibis>0 && jbis<n){
-//                if(0<=ibis<n-1 && 0<=jbis<n-1){
-////                    bool test = ibis<3;
-////                    printf("test : ibis<3 : %d\n", test);
-////                    printf("comparing %c and %c. pos i = %d, pos j = %d\n", a[ibis][jbis], a[ibis-1][jbis+1], ibis, jbis);
-//                    printf("couple (i,j) = (%d,%d)\n", ibis, jbis);
-//                    if (a[ibis][jbis] == a[ibis-1][jbis+1]){
-//                        cntr++;
-//                    }
-//                    else if(cntr>0){
-//                        num_palins++;
-//                        cntr=0;
-//                    }
-//
-//                }
-////                printf("couple (i,j) = (%d,%d)\n", ibis, jbis);
-//                ibis--;
-//                jbis++;
-//            }
-////            printf("sortie de boucle\n");
-//            j++;
-//            if(i!=n)
-//                e++;
-//
-//
-//        }
-//        e=0;
-////        printf("sortie de boucle 2\n");
-//        j=i-1;
-//
-//
-//        if(cntr>0){
-//            cntr=0;
-//            num_palins++;
-//        }
-//    }
-
     int j_scale = 0;
+    ind_palin = 0;
     int n = 1;
     i = 1;
     j = 0;
     int lim = ROWS;
 
+#pragma omp parallel while private(i,j) shared(ind_palin,num_palins, lim, n)
     while(n<lim){
         while(i>j_scale){
-            printf("Couple (i,j) = (%d,%d)\n", i, j);
-            printf("comparing %c and %c. pos i = %d, pos j = %d\n", a[i][j], a[i-1][j+1], j, i);
+//            printf("Couple (i,j) = (%d,%d)\n", i, j);
+            printf("comparing %c and %c. pos i = %d, pos j = %d\n", a[i][j], a[j][i], j, i);
+
+            if(a[i][j] == a[j][i] && i!=j){
+                ind_palin++;
+            } else if(ind_palin>0){
+                num_palins++;
+                ind_palin=0;
+            }
 
             j++;
             i--;
@@ -178,8 +158,9 @@ int main(){
         if(j_scale==lim-2){
             break;
         }
-        printf("n : %d\n", n);
+//        printf("n : %d\n", n);
         if(n<lim-1){
+
             n++;
             i=n;
             j=0;
@@ -195,9 +176,19 @@ int main(){
 
     }
 
+    if(ind_palin>0){
+        num_palins++;
+        ind_palin=0;
+    }
+
+    gettimeofday(&tv2, &tz);
+
+    elapsed = (double) (tv2.tv_sec-tv1.tv_sec) + (double) (tv2.tv_usec-tv1.tv_usec) * 1.e-6;
 
 
-    printf("nb palindrome linéaire : %d", num_palins);
+
+    printf("nb palindrome linéaire : %d\n", num_palins);
+    printf("time elapsed : %f\n", elapsed);
 
     return(0);
 
